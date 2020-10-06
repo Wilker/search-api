0c9580cc7d2dabf93679485f94add75e632c9d23
Wilker <Wilker.rj@gmail.com>
2020-10-06 17:41:44 -0300

    Adding bad request on param missing

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,8 @@
 module Api::V1
   class ApiController < ApplicationController
+    rescue_from(ActionController::ParameterMissing) do
+      render json: {"status": 400,
+                    "error": "Bad Request"}, status: :bad_request
+    end
   end
 end
