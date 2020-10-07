98fa96904bfda4af826a17ff92bd74716b51a045
Wilker <Wilker.rj@gmail.com>
2020-10-07 00:25:04 -0300

    Caching queries on redis

--- app/services/hot_question_services.rb
+++ app/services/hot_question_services.rb
@@ -2,9 +2,22 @@ class HotQuestionServices
   class << self
     def get(params)
       param = convert_params(params)
-      return HotQuestionQuery.get_by_week(param[:year], param[:week]) if param[:week].present?
-      return HotQuestionQuery.get_by_month(param[:year], param[:month]) if param[:month].present?
-      HotQuestionQuery.get_by_year(param[:year]) if param[:year].present?
+
+      if param[:week].present?
+        return Rails.cache.fetch(["by_week", :get]) do
+          HotQuestionQuery.get_by_week(param[:year], param[:week])
+        end
+      end
+
+      if param[:month].present?
+        return Rails.cache.fetch(["by_month", :get]) do
+          HotQuestionQuery.get_by_month(param[:year], param[:month])
+        end
+      end
+
+      Rails.cache.fetch(["by_year", :get]) do
+        HotQuestionQuery.get_by_year(param[:year]) if param[:year].present?
+      end
     end
 
     def convert_params(params)
